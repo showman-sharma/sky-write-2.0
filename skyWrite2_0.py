bdabb5c78ba873922518d92c2d980210c706b58b
Anirudh Sharma <60467702+showman-sharma@users.noreply.github.com>
2021-08-22 23:22:58 +0530

    made some minor changes and removed unwanted code

--- skyWrite2_0.py
+++ skyWrite2_0.py
@@ -19,52 +19,13 @@ frameWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 pts = deque()
 color = deque()
 
-#Saving the video
-#size = (int(cap.get(3)),int(cap.get(4)))
-#recording = cv2.VideoWriter('skywrite2.avi',  
-                         #cv2.VideoWriter_fourcc(*'MJPG'), 
-                         #10, size) 
-
-# For static images:
-IMAGE_FILES = []
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    normalizedLandmark = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-    pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
-                                                                              normalizedLandmark.y,
-                                                                                      frameWidth,
-                                                                                      frameHeight)
- 
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      '''  
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-      '''    
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-
-    
+'''
+#Recording the video
+size = (int(cap.get(3)),int(cap.get(4)))
+recording = cv2.VideoWriter('skywrite2.avi',  
+                         cv2.VideoWriter_fourcc(*'MJPG'), 
+                         10, size) 
+''' 
 #dipping centers
 dipRad = 40
 RC = (290,50) 
@@ -76,6 +37,7 @@ Green = (0,255,0)
 Blue = (255,0,0)
 White = (255,255,255)
 penColor = (0,0,255) #red
+thickness = 10
 frameHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 with mp_hands.Hands(
     min_detection_confidence=0.5,
@@ -100,6 +62,7 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
+        #to draw the hand structure, too
         '''
         mp_drawing.draw_landmarks(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
@@ -145,7 +108,7 @@ with mp_hands.Hands(
                 color = deque()    
         except:
             pass
-        #color Dipping
+        
         
             
      # loop over the set of tracked points
@@ -154,14 +117,10 @@ with mp_hands.Hands(
         # them
         if pts[i - 1] is None or pts[i] is None:
             continue
-         #otherwise, compute the thickness of the line and
-         #draw the connecting lines
-        thickness = 10
+         #otherwise, draw the connecting lines
         cv2.line(image, pts[i -  1], pts[i], color[i], thickness)
-        #try:
-            #cv2.circle(image, pts[i], 5, color[i], -1)
-        #except:
-            #continue
+        
+    #color Dipping        
     if(penColor == Red):
         header = overlayList[0]
     elif(penColor == Green):
@@ -176,10 +135,11 @@ with mp_hands.Hands(
     except:
         continue
     cv2.imshow('MediaPipe Hands', image)
-    #for saving the video
+    
     #recording.write(image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
+    
 cap.release()
 #recording.release() 
 cv2.destroyAllWindows()
